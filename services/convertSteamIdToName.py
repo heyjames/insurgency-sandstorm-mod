1bd54cd02ca7f21db51f7533fd78268868c6f211
heyjames <heyjameslam@gmail.com>
2019-10-04 15:21:18 -0700

    Change the way env vars are set

--- services/convertSteamIdToName.py
+++ services/convertSteamIdToName.py
@@ -1,7 +1,7 @@
-import urllib, json, sys
+import urllib, json, sys, os
 
 try:
-    url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v1/?key=xxxxxxxxxx&steamids=" + sys.argv[1]
+    url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v1/?key=" + os.environ['STEAM_API_KEY'] + "&steamids=" + sys.argv[1]
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     print(data['response']['players']['player'][0]['personaname'])
