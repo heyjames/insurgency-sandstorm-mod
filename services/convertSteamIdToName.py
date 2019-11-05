be7283b70ce577877ef2b6692f50c26114842671
heyjames <heyjameslam@gmail.com>
2019-11-04 19:20:23 -0800

    Various bug fixes and improvements. Add new single, refactored, event-driven formatted script.

--- services/convertSteamIdToName.py
+++ services/convertSteamIdToName.py
@@ -1,9 +1,13 @@
 import urllib, json, sys, os
 
 try:
-    url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v1/?key=" + os.environ['STEAM_API_KEY'] + "&steamids=" + sys.argv[1]
+    # url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v1/?key=" + os.environ['STEAM_API_KEY'] + "&steamids=" + sys.argv[1]
+    url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v1/?key=xxxxxxxxxx&steamids=" + sys.argv[1]
     response = urllib.urlopen(url)
     data = json.loads(response.read())
-    print(data['response']['players']['player'][0]['personaname'])
+    # print(data['response']['players']['player'][0]['personaname'])
+    name = data['response']['players']['player'][0]['personaname']
+    # name = name.replace(u"\u2122", '')
+    print(name)
 except:
     print("Error")
